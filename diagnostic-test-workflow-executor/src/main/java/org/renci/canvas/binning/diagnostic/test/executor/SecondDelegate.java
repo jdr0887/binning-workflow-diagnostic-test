866a5378734d7387abf15af1ded85330dfaf5b14
Jason Reilly <jdr0887@gmail.com>
2017-05-25 14:33:55 -0400

    initial commit

---
+++ diagnostic-test-workflow-executor/src/main/java/org/renci/canvas/binning/diagnostic/test/executor/SecondDelegate.java
@@ -0,0 +1,23 @@
+package org.renci.canvas.binning.diagnostic.test.executor;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SecondDelegate implements JavaDelegate {
+
+    private static final Logger logger = LoggerFactory.getLogger(SecondDelegate.class);
+
+    public SecondDelegate() {
+        super();
+    }
+
+    @Override
+    public void execute(DelegateExecution arg0) throws Exception {
+        for (int i = 0; i < 10000000; i++) {
+            logger.info("i = {}", i);
+        }
+    }
+
+}
